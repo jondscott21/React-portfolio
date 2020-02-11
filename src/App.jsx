c058182172126caabf185c2146a4b571c0c9fcbb
Jonathan Scott <jonathan.scott@gmail.com>
2020-02-10 22:58:14 -0800

    modified some yarn and css settings

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+// import {useSelect, useDispatch} from 'react-redux';
+
+function App() {
+  // const state = useSelect(state => state)
+  // const dispatch = useDispatch()
+  return (
+    <div className="App">
+      <div className="App-landing">Jonathan Scott</div>
+    </div>
+  );
+}
+
+export default App;
