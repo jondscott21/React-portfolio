c6650d5bc1ecc4f0df877e1b85349ed06bbe28ef
Jonathan Scott <jonathan.scott@gmail.com>
2020-02-18 14:59:38 -0800

    added phone scaling for resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import MyResume from "../assets/jonathan-resume.pdf"
 import {StyledIFrame, IFrameWrapper} from '../styles/ResumeStyle'
 
 
 const Resume = () => {
+    const [percent, setPercent] = useState(100)
+    useEffect(() => {
+        if (window.innerWidth === 500) {
+            setPercent(57)
+        }
+    },[window.innerWidth])
     return(
         // <IFrameWrapper>
         //     <StyledIFrame  title="Jonathan Scott's Resume" src={MyResume} />
         // </IFrameWrapper>
-        <StyledIFrame  title="Jonathan Scott's Resume" src={MyResume} />
+        <StyledIFrame  title="Jonathan Scott's Resume" src={`${MyResume}#zoom=${percent}`} />
     )   
 }
 
--- src/styles/ResumeStyle.js
+++ src/styles/ResumeStyle.js
@@ -13,13 +13,17 @@ export const IFrameWrapper = styled.div`
 `
 
 export const StyledIFrame = styled.iframe`
-    min-width: 99%;
-    width: 1px;
+    margin-top: 60px;
+    min-width: 99.5%;
+    /* width: 1px; */
     height: 100%;
     min-height: 1180px;
     /* white-space: nowrap */
-    @media (max-width: 500px) {
-        /* width: 70%; */
+    /* @media (max-width: 500px) {
+        min-width: 490px;
+        min-height: 590px;
+        height: 400px;
+        width: 400px;
         /* white-space: nowrap */
-    }
+    } */
 `
