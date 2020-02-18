b4516f9cad27b8f9ba6ff5848e42fc806b447502
Jonathan Scott <jonathan.scott@gmail.com>
2020-02-18 14:32:38 -0800

    more resume iframe styling tweaks

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import MyResume from "../assets/jonathan-resume.pdf"
-import {StyledIFrame} from '../styles/ResumeStyle'
+import {StyledIFrame, IFrameWrapper} from '../styles/ResumeStyle'
 
 
 const Resume = () => {
     return(
+        // <IFrameWrapper>
+        //     <StyledIFrame  title="Jonathan Scott's Resume" src={MyResume} />
+        // </IFrameWrapper>
         <StyledIFrame  title="Jonathan Scott's Resume" src={MyResume} />
     )   
 }
--- src/styles/ResumeStyle.js
+++ src/styles/ResumeStyle.js
@@ -1,9 +1,25 @@
 import styled from 'styled-components'
 
-export const StyledIFrame = styled.iframe`
-    margin-top: 80px;
-    min-height: 800px;
+
+export const IFrameWrapper = styled.div`
+    margin-top: 60px;
+   
     /* min-width: 490px; */
-    width: 100%;
+    width: 99.5%;
     height: 100%;
+    /* @media (max-width: 500px) {
+        width: 70%;
+    } */
+`
+
+export const StyledIFrame = styled.iframe`
+    min-width: 99%;
+    width: 1px;
+    height: 100%;
+    min-height: 1180px;
+    /* white-space: nowrap */
+    @media (max-width: 500px) {
+        /* width: 70%; */
+        /* white-space: nowrap */
+    }
 `
