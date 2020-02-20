902cf000bfd6ca5ff0e3b8ded9150e64a370a5a9
Jonathan Scott <jonathan.scott@gmail.com>
2020-02-20 13:27:35 -0800

    reworked iframe issue

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,29 +1,31 @@
 import React, {useState, useEffect} from 'react'
 import MyResume from "../assets/jonathan-resume.pdf"
-import { StyledIFrame } from '../styles/ResumeStyle'
+import { StyledMobileIFrame, StyledDesktopIFrame } from '../styles/ResumeStyle'
 
 
 const Resume = () => {
     const [url, setUrl] = useState(`${MyResume}#zoom=100`)
     const [loading, setLoading] = useState(false)
-    useEffect(() => {
-       window.addEventListener('resize', handleResize)
-    },[])
-    const handleResize = () => {
-        if (window.visualViewport.width <= 500) {
-            setLoading(true)
-            setUrl(`${MyResume}#zoom=57`)
-            setLoading(false)
-        }
-        else if (window.visualViewport.width > 500) {
-            setLoading(true)
-            setUrl(`${MyResume}#zoom=100`)
-            setLoading(false)
-        }
-    }
+    // useEffect(() => {
+    //    window.addEventListener('resize', handleResize)
+    // },[])
+    // const handleResize = () => {
+    //     if (window.visualViewport.width <= 600) {
+    //         setLoading(true)
+    //         setUrl(`${MyResume}#zoom=57`)
+    //         setLoading(false)
+    //     }
+    //     else if (window.visualViewport.width > 600) {
+    //         setLoading(true)
+    //         setUrl(`${MyResume}#zoom=100`)
+    //         setLoading(false)
+    //     }
+    // }
     return(
         <>
-            {!loading ? <StyledIFrame title="Jonathan Scott's Resume" src={url}  /> : null}
+            {/* {!loading ? <StyledIFrame title="Jonathan Scott's Resume" src={url}  /> : null} */}
+            <StyledDesktopIFrame className='desktop' title="Jonathan Scott's Resume" src={`${MyResume}#zoom=100`} />
+            <StyledMobileIFrame className='mobile' title="Jonathan Scott's Resume" src={`${MyResume}#zoom=57`} />
         </>
     )   
 }
--- src/styles/ResumeStyle.js
+++ src/styles/ResumeStyle.js
@@ -1,12 +1,27 @@
 import styled from 'styled-components'
 
 
-export const StyledIFrame = styled.iframe`
+export const StyledDesktopIFrame = styled.iframe`
     margin-top: 60px;
     min-width: 99.5%;
     width: 1px;
     min-height: 1180px;
     @media (max-width: 500px) {
         min-width: 99%;
+        width: 480px;
+        display: none
+
+    }
+`
+export const StyledMobileIFrame = styled.iframe`
+    margin-top: 60px;
+    min-width: 99.5%;
+    width: 1px;
+    min-height: 1180px;
+    display: none;
+    @media (max-width: 500px) {
+        min-width: 99%;
+        width: 480px;
+        display: block
     }
 `
