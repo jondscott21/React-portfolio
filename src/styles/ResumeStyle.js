8ffbade9e6fa34f57b459bc137e15c74e5efb3da
Jonathan Scott <jonathan.scott@gmail.com>
2020-02-20 15:54:09 -0800

    more resume tweaking

--- src/styles/ResumeStyle.js
+++ src/styles/ResumeStyle.js
@@ -6,17 +6,17 @@ export const StyledDesktopIFrame = styled.iframe`
     min-width: 99.5%;
     width: 1px;
     min-height: 1180px;
-    @media (max-width: 550px) {
+    @media only screen and (max-device-width: 550px) {
         display: none
     }
 `
 export const StyledMobileIFrame = styled.iframe`
         margin-top: 80px;
-        min-width: 99%;
-        width: 1px;
-        min-height: 1180px;
+        min-width: 90%;
+        /* width: 1px; */
+        min-height: 812px;
         display: none;
-    @media (max-width: 550px) {
+    @media only screen and (max-device-width: 550px) {
         display: block;
     }
 `
